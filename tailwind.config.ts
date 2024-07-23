5f034f998b1427dce8ba26a1058b655bac44b42e
Thanh Hùng Lý <thanhhung270502@gmail.com>
2024-07-23 18:22:01 +0700

    Initial Project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
       },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
+      }
     },
   },
   plugins: [],
